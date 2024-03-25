01971c460cb3641b91054ba47ad46d14bead8560
dariushghasemi <ghasemi.dariush@yahoo.com>
2024-03-25 23:54:14 +0100

    Including format conversion to rules!

--- bin/04_compute_ld.sh
+++ bin/04_compute_ld.sh
@@ -7,20 +7,21 @@
 #SBATCH --mem 128G
 #SBATCH --time 100:00:00
 
-genotype=/processing_data/shared_datasets/plasma_proteome/interval/genotypes
+#genotype=/processing_data/shared_datasets/plasma_proteome/interval/genotypes
+genotype=/scratch/giulia.pontali/test_snakemake/genomics_QC_pipeline/results/pgen
 base=/home/dariush.ghasemi/projects/pqtl_pipeline_ldsc
 ldsc=/home/dariush.ghasemi/bin/ldsc
 
 source ~/.bashrc
 conda activate ldsc
-
+#merged_imputation  \
 ldsc.py  \
-	--bfile $genotype/merged_imputation  \
+	--bfile $genotype/impute_recoded_selected_sample_filter_hq_var_21  \  
 	--l2  \
     --maf 0.05  \
 	--ld-wind-cm 1  \
 	--yes-really  \
-	--out $base/output/ld/ld.merged_imputation
+	--out $base/output/ld/ld.chr22
 
 #sbatch 04_compute_ld.sh -p cpuq  --mem-per-cpu=16GB -J comp_ld.sh
 
